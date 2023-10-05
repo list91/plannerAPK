a27a81fde83fefc28c2aedba36d8fb00b249afa7
list91 <s_anufriev@list.ru>
2023-10-05 12:10:15 +0700

    добавил вкладки

--- app/src/main/java/com/example/planer/MainActivity.java
+++ app/src/main/java/com/example/planer/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.planer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.TableRow;
 import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -44,6 +52,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Context context = getApplicationContext();
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        TabHost tabHost = findViewById(android.R.id.tabhost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec;
+
+// Создание вкладки "Не срочно"
+        tabSpec = tabHost.newTabSpec("Tab1");
+        tabSpec.setIndicator("Не срочно");
+        tabSpec.setContent(R.id.blueBlock);
+        tabHost.addTab(tabSpec);
+
+// Создание вкладки "Срочно"
+        tabSpec = tabHost.newTabSpec("Tab2");
+        tabSpec.setIndicator("Срочно");
+        tabSpec.setContent(R.id.greenBlock);
+        tabHost.addTab(tabSpec);
+
+// Создание вкладки "Очень срочно"
+        tabSpec = tabHost.newTabSpec("Tab3");
+        tabSpec.setIndicator("Очень срочно");
+        tabSpec.setContent(R.id.yellowBlock);
+        tabHost.addTab(tabSpec);
+
+// Установка первой вкладки активной
+        tabHost.setCurrentTab(0);
+
 //        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView errorTextView = findViewById(R.id.infoLabel); // Идентификатор текстового поля
 
         JsonFile.createDefaultOutJsonFile(getApplicationContext());
@@ -69,13 +105,17 @@ public class MainActivity extends AppCompatActivity {
         objectParams = new ObjectParams(null, null, "помыть посуду", "помой посуду хорошо", "без категории", priorityEasy, null);
         Object newObject7 = new Object(getApplicationContext(), objectParams);
 
-        newObject.writeObject();
+        try {
+            newObject.writeObject();
         newObject2.writeObject();
         newObject3.writeObject();
         newObject4.writeObject();
         newObject5.writeObject();
         newObject6.writeObject();
         newObject7.writeObject();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
 
         LinearLayout linearLayoutNESROCHNO = findViewById(R.id.linearLayoutBlue);
         LinearLayout linearLayoutSROCHNO = findViewById(R.id.linearLayoutGreen);
@@ -102,6 +142,9 @@ public class MainActivity extends AppCompatActivity {
             assert priority != null;
             setObjectInInterface(item, priority);
         }
+
+
+
 //        for (JSONObject value: values){
 
 //        }
@@ -119,15 +162,43 @@ public class MainActivity extends AppCompatActivity {
         View scrollViewView = inflater.inflate(R.layout.item_scroll_view, (ViewGroup) thisLinearLayout, false);
 
         // Модифицируем содержимое scrollViewView
-        @SuppressLint("CutPasteId") TextView textViewTitle = scrollViewView.findViewById(R.id.titleLabel);
-        TextView textViewDescription = scrollViewView.findViewById(R.id.taskDescriptionLabel);
-        TextView textViewDate = scrollViewView.findViewById(R.id.dateLabel);
-        @SuppressLint("CutPasteId") TextView textViewTime = scrollViewView.findViewById(R.id.timeLabel);
+        @SuppressLint({"CutPasteId", "MissingInflatedId", "LocalSuppress"}) TextView textViewTitle = scrollViewView.findViewById(R.id.labelTitle);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView textViewDescription = scrollViewView.findViewById(R.id.labelDescription);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView textViewDate = scrollViewView.findViewById(R.id.labelDateTime);
+//        @SuppressLint("CutPasteId") TextView textViewTime = scrollViewView.findViewById(R.id.timeLabel);
 
         textViewTitle.setText(objectParams.getTitle());
-        textViewDescription.setText(objectParams.getContent());
+//        textViewDescription.setText(objectParams.getContent());
         textViewDate.setText(objectParams.getDate());
-        textViewTime.setText(objectParams.getTime());
+//        textViewTime.setText(objectParams.getTime());
+
+        TableRow row2 = scrollViewView.findViewById(R.id.row2);
+        int targetHeight = 100; // Здесь можно установить целевую высоту строки
+        row2.getLayoutParams().height = 0; // устанавливаем начальную высоту строки равной 0
+        row2.requestLayout();
+        ValueAnimator animator = ValueAnimator.ofInt(0, targetHeight); // создаем аниматор для изменения высоты
+        animator.setDuration(3000); // задаем продолжительность анимации
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int value = (int) animation.getAnimatedValue(); // получаем текущее значение анимации
+                row2.getLayoutParams().height = value; // устанавливаем новую высоту строки
+                row2.requestLayout();
+            }
+        });
+
+// Установка продолжительности анимации
+//        animation.setDuration(3000); // Здесь можно установить желаемую продолжительность анимации
+
+// Запуск анимации по нажатию на кнопку
+        ImageButton button = scrollViewView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                row2.setVisibility(View.VISIBLE); // Показывает вторую строку перед запуском анимации
+                animator.start(); // Запускает анимацию изменения высоты строки
+            }
+        });
 
 //        textView.setText(txt);
 
